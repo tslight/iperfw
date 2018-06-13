3ccdb7f8abcaa52f35a3ee61dd2509d53b34b4a1
tslight <tobyslight@gmail.com>
2018-06-13 09:03:13 +0100

    added favicon and removed unused classes

--- index.php
+++ index.php
@@ -1,42 +1,43 @@
 <?php require 'functions.php' ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>SPEEDTEST UTILS</title>
+    <title>NET UTILS</title>
     <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="icon" href="img/nodes.png" type="image/png">
   </head>
-
   <body>
+
     <div class="row">
+
       <div class="column left">
 	<h1>IPERF</h1>
 	<form method="post">
 	  <input name="type" type="hidden" value="iperf"/>
 
-	  <label for="version" class="label">Version</label>
+	  <label for="version">Version</label>
 	  <select id="version" name="version">
 	    <option value="2" selected="selected">2</option>
 	    <option value="3" >3</option>
 	  </select><br>
 
-	  <label for="target" class="label">Target</label>
+	  <label for="target">Target</label>
 	  <input id="target" name="target" type="text" maxlength="255" value="ping.online.net"/><br>
 
-	  <label for="port" class="label">Port</label>
+	  <label for="port">Port</label>
 	  <input id="port" name="port" type="text" maxlength="255" value="5001"/><br>
 
-	  <label for="type" class="label">Protocol</label>
+	  <label for="type">Protocol</label>
 	  <select id="proto" name="proto">
 	    <option value="tcp" selected="selected">TCP</option>
 	    <option value="udp" >UDP</option>
 	  </select><br>
 
-	  <label for="params" class="label">Parameters</label>
+	  <label for="params">Parameters</label>
 	  <input id="params" name="params" type="text" maxlength="255" value="-i 1 -f -m"/><br>
 
-	  <label for="timeout" class="label">Timeout</label>
+	  <label for="timeout">Timeout</label>
 	  <input id="timeout" name="timeout" type="text" maxlength="3" value="15"/><br>
 
 	  <input type="submit" name="Start" value="Start" />
@@ -46,10 +47,10 @@
 	<form method="post">
 	  <input name="type" type="hidden" value="ping"/>
 
-	  <label for="target" class="label">Target:</label>
+	  <label for="target">Target:</label>
 	  <input id="target" name="target" type="text" maxlength="255" value="8.8.8.8"/><br>
 
-	  <label for="params" class="label">Parameters:</label>
+	  <label for="params">Parameters:</label>
 	  <input id="params" name="params" type="text" maxlength="255" value="-c 4"/><br>
 
 	  <input type="submit" name="Start" value="Start" />
@@ -59,10 +60,10 @@
 	<form method="post"">
 	  <input name="type" type="hidden" value="traceroute"/>
 
-	  <label for="target" class="label">Target:</label>
+	  <label for="target">Target:</label>
 	  <input id="target" name="target" type="text" maxlength="255" value="8.8.8.8"/><br>
 
-	  <label for="params" class="label">Parameters:</label>
+	  <label for="params">Parameters:</label>
 	  <input id="params" name="params" type="text" maxlength="255" value=""/><br>
 
 	  <input type="submit" name="Start" value="Start"/>
@@ -73,9 +74,11 @@
 	<h1>RESULT</h1>
 	<?php getResults() ?>
       </div>
+
     </div>
 
     <div class="row">
+
       <div class="column left">
 	<h1>LOGS</h1>
 	<table>
@@ -90,6 +93,7 @@
 	<h1>LOG VIEWER</h1>
 	<?php getLogContent() ?>
       </div>
+
     </div>
 
   </body>
